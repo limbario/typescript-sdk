78d02b40491709e8c9db477d2e0fe5482e40ee44
Karl Baumgartner <karl@limbar.io>
2025-05-24 13:29:29 +0300

    ui: include css in the bundle
    Signed-off-by: Karl Baumgartner <karl@limbar.io>

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({ include: ['src'] })
   ],
   build: {
@@ -16,6 +18,7 @@ export default defineConfig({
       formats: ['es', 'cjs'],
       fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
     },
+    cssCodeSplit: false,
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
